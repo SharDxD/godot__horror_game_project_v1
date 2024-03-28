dbfa407e8ffbfbd8ae7588fe3d07daeffb6ffd0c
SharDxD <kirigolub@gmail.com>
2024-03-28 18:36:40 +0100

    Initial commit

---
+++ main_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var button_pressed : bool
+var toggled_on : bool
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level_1_test.tscn")
+	
+func _on_end_game_pressed():
+	get_tree().quit()
+
+func _on_music_switch_button_down():
+	$AudioStreamPlayer.stop()
+
+
+func _on_music_switch_button_up():
+	$AudioStreamPlayer.play()
+
+#
+
+func _on_music_switch_toggled(button_pressed):
+	if button_pressed:
+		$AudioStreamPlayer2D.play()
+	else:
+		$AudioStreamPlayer2D.stop()
+		
+
+
+
+func _on_fullscreen_switch_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(3, 0)
+	else:
+		DisplayServer.window_set_mode(0, 0)
